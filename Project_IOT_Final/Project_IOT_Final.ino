afa8cb308858944a898a2694d3394647adf29b4a
Navindu Dissanayake <navindu158@gmail.com>
2024-03-01 23:29:38 +0530

    file change

--- Project_IOT_Final/Project_IOT_Final.ino
+++ Project_IOT_Final/Project_IOT_Final.ino
@@ -10,8 +10,11 @@ int engineStatus = 0;
 int buttonNew;
 int buttonOld = 1;
 bool alcoholDetected = false;
+int sensorInitialTimeCount = 0;
 
 int engineTerminatingTime = 10;
+int sensorTimeCount = 3;
+
 
 void setup() {
 
@@ -34,64 +37,69 @@ void loop() {
 
 
   if (gasValue > 200) {
-    digitalWrite(12, HIGH);
 
-    if(engineStatus == 0){
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Alcohol Detected");
+    sensorInitialTimeCount++;
 
-      delay(2000);
+    if (sensorInitialTimeCount >= (sensorTimeCount * 5)) {
+      digitalWrite(12, HIGH);
 
-      lcd.setCursor(0, 0);
-      lcd.print("Engine Starting");
-      lcd.setCursor(0, 1);
-      lcd.print("Disengaged");
-
-      delay(2000);
-      
-    }else{
-      for(int i = 0; i<6; i++){
+      if (engineStatus == 0) {
         lcd.clear();
         lcd.setCursor(0, 0);
         lcd.print("Alcohol Detected");
-        delay(1000);
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("Stop the Vehical");
-        lcd.setCursor(2, 1);
-        lcd.print("Immediately");
-        delay(1000);
-      }
 
-      for(int i = engineTerminatingTime; i>=0; i--){
-        lcd.clear();
+        delay(2000);
+
         lcd.setCursor(0, 0);
-        lcd.print("Engine Terminate");
+        lcd.print("Engine Starting");
         lcd.setCursor(0, 1);
-        lcd.print("in ");
-        lcd.setCursor(4, 1);
-        lcd.print(i);
-        delay(1000);
+        lcd.print("Disengaged");
+
+        delay(2000);
+
+      } else {
+        for (int i = 0; i < 6; i++) {
+          lcd.clear();
+          lcd.setCursor(0, 0);
+          lcd.print("Alcohol Detected");
+          delay(1000);
+          lcd.clear();
+          lcd.setCursor(0, 0);
+          lcd.print("Stop the Vehical");
+          lcd.setCursor(2, 1);
+          lcd.print("Immediately");
+          delay(1000);
+        }
+
+        for (int i = engineTerminatingTime; i >= 0; i--) {
+          lcd.clear();
+          lcd.setCursor(0, 0);
+          lcd.print("Engine Terminate");
+          lcd.setCursor(0, 1);
+          lcd.print("in ");
+          lcd.setCursor(4, 1);
+          lcd.print(i);
+          delay(1000);
+        }
+
+        digitalWrite(engine, 0);
+        engineStatus = 0;
       }
 
-      digitalWrite(engine, 0);
-      engineStatus = 0;
-
+      alcoholDetected = true;
     }
-
-    alcoholDetected = true;
 
   } else {
     digitalWrite(12, LOW);
 
-    if(alcoholDetected){
+    if (alcoholDetected) {
+      sensorInitialTimeCount = 0;
       lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print("System Preparing");
       delay(500);
       lcd.setCursor(0, 1);
-      for(int i = 0; i<17; i++){
+      for (int i = 0; i < 17; i++) {
         lcd.print(".");
         delay(150);
       }
@@ -138,13 +146,5 @@ void loop() {
     buttonOld = buttonNew;
   }
 
-  delay(100);
+  delay(200);
 }
-
-
-
-
-
-
-
-
